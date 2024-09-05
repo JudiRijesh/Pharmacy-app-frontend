1b3ec721bb1b886f3f917c3ebe3f75790ba91310
Mary Judi Kavitha Karunakaran <kavitha.rijesh@gmail.com>
2024-09-05 21:08:14 +0200

    Final Updates

--- src/Pages/BookLabTests.jsx
+++ src/Pages/BookLabTests.jsx
@@ -20,7 +20,7 @@ import Wiron from '../assets/Test/Wiron.png'
 function BookLabTests() {
   return (
     <div>
-    <h5 className='doctor'>Doctor Created Health Checks</h5>
+    <h6 className='doctor'>Doctor Created Health Checks</h6>
     
 
     <div className="item-grid">
@@ -74,7 +74,7 @@ function BookLabTests() {
     </div>
   </div>
 
-  <h5 className='doctor'>Women Care</h5>
+  <h6 className='doctor'>Women Care</h6>
   <div className="item-grid">
   <div className="item-box">
   <img src={Women} alt="Women" className="item-image" />
@@ -103,28 +103,28 @@ function BookLabTests() {
   </div>
   </div>
 
-  <h5 className='doctorl'>Care diagnostic nearby you that you can trust</h5>
-  <p>Lab tests are one of the most critical factors in determining one's health. Being the backbone of the Indian healthcare sector, we at Care 24|7 bring a plethora of lab tests at home for you to choose from Care Diagnostics including a blood test at home.
+  <h6 className='doctorl'>MAD diagnostic nearby you that you can trust</h6>
+  <p>Lab tests are one of the most critical factors in determining one's health. Being the backbone of the Indian healthMAD sector, we at MAD 24|7 bring a plethora of lab tests at home for you to choose from MAD Diagnostics including a blood test at home.
 
-You can now access Care diagnostic services conveniently wrapped in an intuitive app, from which you can book lab test online with just a few clicks.
+You can now access MAD diagnostic services conveniently wrapped in an intuitive app, from which you can book lab test online with just a few clicks.
 
 Getting a diagnostic test done is no longer going to be a troublesome process involving long waiting times, trips to the lab and delayed reports. We have made the process super-smooth for you by facilitating easy blood sample collection from home, speedy report delivery and exclusive discounts. Also, our diagnostic package, lab test package and blood test package are priced at a competitive rate, so that you can book blood test online without burning a hole in your pocket.</p>
 
-<h5 className='doctorl'>Why Choose Care 24|7 for a Lab Test at Home? </h5>
-<p>Being the pioneers in the healthcare sector for the last 36 years, the Care family has a legacy of providing excellence in all aspects of healthcare blood tests at home. Carrying the same ethos of quality healthcare forward, at Care 24|7, we ensure that we deliver the topmost quality diagnostic tests and results. This is why more than 3.5 million customers get their diagnostic tests done from Care Diagnostic centers every year, setting benchmarks in quality healthcare. Care has curved a niche for itself in the diagnostic package, lab test package and blood test package segment. They have made the home blood collection procedure extremely user-friendly and smooth.</p>
+<h6 className='doctorl'>Why Choose MAD 24|7 for a Lab Test at Home? </h6>
+<p>Being the pioneers in the healthMAD sector for the last 36 years, the MAD family has a legacy of providing excellence in all aspects of healthMAD blood tests at home. Carrying the same ethos of quality healthMAD forward, at MAD 24|7, we ensure that we deliver the topmost quality diagnostic tests and results. This is why more than 3.5 million customers get their diagnostic tests done from MAD Diagnostic centers every year, setting benchmarks in quality healthMAD. MAD has curved a niche for itself in the diagnostic package, lab test package and blood test package segment. They have made the home blood collection procedure extremely user-friendly and smooth.</p>
 
-<h5 className='doctorl'>Fast Sample Collection & Report Delivery by Care Diagnostic</h5>
-<p>Having 100+ Care Diagnostics centres back in 2015, we have grown to have 1200 Patient Care Centres and 120 labs in 2022. With the foundation of 700+ collection centres and 100+ laboratories, we have made omnichannel healthcare delivery a reality in India. After you book your home lab test, diagnostic package or blood test package, we facilitate your sample collection from home in 30 minutes and same-day report delivery for most of the tests. We have been adding newer and better radiological and other lab test equipment throughout the years to provide exceptional quality lab tests & results from the Care pathology lab. The next time you need to search ‘pathology labs near me’, or ‘blood test near me’, just head to Care 24|7 and book lab tests online.</p>
+<h6 className='doctorl'>Fast Sample Collection & Report Delivery by MAD Diagnostic</h6>
+<p>Having 100+ MAD Diagnostics centres back in 2015, we have grown to have 1200 Patient MAD Centres and 120 labs in 2022. With the foundation of 700+ collection centres and 100+ laboratories, we have made omnichannel healthMAD delivery a reality in India. After you book your home lab test, diagnostic package or blood test package, we facilitate your sample collection from home in 30 minutes and same-day report delivery for most of the tests. We have been adding newer and better radiological and other lab test equipment throughout the years to provide exceptional quality lab tests & results from the MAD pathology lab. The next time you need to search ‘pathology labs near me’, or ‘blood test near me’, just head to MAD 24|7 and book lab tests online.</p>
 
-<h5 className='doctorl'>Value for Money</h5>
-<p>With exclusive discounts on lab tests and Health Credits on every test, we make sure that our blood tests are accessible and affordable. We also offer flexible modes of payment, including cash on delivery (COD). Care 24|7 is on a mission to provide affordable lab tests at home. </p>
+<h6 className='doctorl'>Value for Money</h6>
+<p>With exclusive discounts on lab tests and Health Credits on every test, we make sure that our blood tests are accessible and affordable. We also offer flexible modes of payment, including cash on delivery (COD). MAD 24|7 is on a mission to provide affordable lab tests at home. </p>
 
-<h5 className='doctorl'>Professionally Trained Technicians for Home Sample Collection</h5>
-<p>Qualified and trained phlebotomists from our NABL certified Care Diagnostic Labs and collection centres, come to your doorstep to collect the samples in a safe, hygienic and hassle-free manner. Care 24|7 brings the best of Care Diagnostics to your home for all your diagnostic requirements. 
+<h6 className='doctorl'>Professionally Trained Technicians for Home Sample Collection</h6>
+<p>Qualified and trained phlebotomists from our NABL certified MAD Diagnostic Labs and collection centres, come to your doorstep to collect the samples in a safe, hygienic and hassle-free manner. MAD 24|7 brings the best of MAD Diagnostics to your home for all your diagnostic requirements. 
 A well-spread network of 700+ collection centres and 100+ laboratories
 Top-notch quality and accurate online blood tests at unbelievable discounts
 Cutting-edge technology and modern equipments
-Priority access and best deals with Care Circle membership on every lab test at home
+Priority access and best deals with MAD Circle membership on every lab test at home
 An exquisite and intricate value system
 High-quality diagnostic services and sample collection
 Rigid internal & external quality supervision
@@ -132,21 +132,21 @@ Continuous upgradation & addition of the latest technologies
 Easy and convenient online blood lab test booking
 Wide range of medical specialties and lab tests at home</p>
 
-<h5 className='doctorl'>When Do You Need to Book Blood Test Online?</h5>
-<p>In a preventive scenario, lab tests are the first line of defence against diseases. Therefore it becomes extremely crucial to get tested by trusted Care pathology labs regularly to detect early-onset conditions which can later become significant health challenges. With at-home sample collection for lab tests, getting preventive health checks is now more convenient than ever.
+<h6 className='doctorl'>When Do You Need to Book Blood Test Online?</h6>
+<p>In a preventive scenario, lab tests are the first line of defence against diseases. Therefore it becomes extremely crucial to get tested by trusted MAD pathology labs regularly to detect early-onset conditions which can later become significant health challenges. With at-home sample collection for lab tests, getting preventive health checks is now more convenient than ever.
 
 If you have already been diagnosed with some disease, then the diagnostic tests will be your biomarker of how well your body is responding to the treatment plan. This, in turn, will assist the doctor in tweaking the treatment plan as and when required. 
 
-It is no secret that regular health checks are vital for a healthy life. Conditions such as diabetes and cardiac issues need regular checkups which becomes a hassle if you have to physically go to the a lab each time. With Care 24|7, you enjoy the liberty of booking a blood test at home. Care has also designed their diagnostic package, lab test package and blood test package keeping the users’ benefits in mind. Also, once you book blood test with Care 24|7, a well-trained phlebotomist will be designated for blood sample collection from home.</p>
+It is no secret that regular health checks are vital for a healthy life. Conditions such as diabetes and cardiac issues need regular checkups which becomes a hassle if you have to physically go to the a lab each time. With MAD 24|7, you enjoy the liberty of booking a blood test at home. MAD has also designed their diagnostic package, lab test package and blood test package keeping the users’ benefits in mind. Also, once you book blood test with MAD 24|7, a well-trained phlebotomist will be designated for blood sample collection from home.</p>
 
 
-<h5 className='doctorl'>A Broad Range of Lab Tests Under One Roof!</h5>
-<p>We believe in timely, quality, and patient-friendly services. Routine tests and health check-ups are vital, so we make sure that we can meet your requirements. We offer a wide range of lab tests Full Body Checkup | Hba1c Test | Vitamin D 25 Hydroxy Test |Cholesterol Test | Complete Urinalysis Test | FBS Test | TSH Test | Liver Function Test | Creatinine Test | CRP Test | Urine Culture Test | KFT Test/ RFT Test | PT INR Test | ESR Test | Beta Hcg Test | Uric Acid Test | Electrolytes Serum Test | Serum Potassium Test | Urea And Creatinine Test | Calcium Serum Test | Sodium Serum Test | HIV Rapid Test | Ferritin Test | Vitamin B12 Test | Hbsag Test | Prolactin Test | RA Factor Test | Anti HCV Test | VDRL Test | SGPT Test | Urine Protein Test | Prostatic Specific Antigen Test | Anti Mullerian Hormone Test | Fasting Insulin Test | Liver Function Test With Ggt | SGOT Ast Test | APTT Test | Total Ige Test | Platelet Count Test  | RT-PCR Test | Renal Profile Test (KFT, RFT) | Hemogram Test | Lipid Profile Test | Thyroid Profile Test (T3 T4 TSH) | D-Dimer Test | Complete Blood Count Test (CBC Test) | Widal Test | Full Body Check up - Basic | Full Body Check up - Advance |  Kidney Test | and more with home blood collection. Our main goal is to focus on the quality care of patients. We believe, for good treatment and care, high-quality diagnostics are vital. At Care Diagnostics, all lab tests facilities are made available so that you can book lab test online without any hassle.
+<h6 className='doctorl'>A Broad Range of Lab Tests Under One Roof!</h6>
+<p>We believe in timely, quality, and patient-friendly services. Routine tests and health check-ups are vital, so we make sure that we can meet your requirements. We offer a wide range of lab tests Full Body Checkup | Hba1c Test | Vitamin D 25 Hydroxy Test |Cholesterol Test | Complete Urinalysis Test | FBS Test | TSH Test | Liver Function Test | Creatinine Test | CRP Test | Urine Culture Test | KFT Test/ RFT Test | PT INR Test | ESR Test | Beta Hcg Test | Uric Acid Test | Electrolytes Serum Test | Serum Potassium Test | Urea And Creatinine Test | Calcium Serum Test | Sodium Serum Test | HIV Rapid Test | Ferritin Test | Vitamin B12 Test | Hbsag Test | Prolactin Test | RA Factor Test | Anti HCV Test | VDRL Test | SGPT Test | Urine Protein Test | Prostatic Specific Antigen Test | Anti Mullerian Hormone Test | Fasting Insulin Test | Liver Function Test With Ggt | SGOT Ast Test | APTT Test | Total Ige Test | Platelet Count Test  | RT-PCR Test | Renal Profile Test (KFT, RFT) | Hemogram Test | Lipid Profile Test | Thyroid Profile Test (T3 T4 TSH) | D-Dimer Test | Complete Blood Count Test (CBC Test) | Widal Test | Full Body Check up - Basic | Full Body Check up - Advance |  Kidney Test | and more with home blood collection. Our main goal is to focus on the quality MAD of patients. We believe, for good treatment and MAD, high-quality diagnostics are vital. At MAD Diagnostics, all lab tests facilities are made available so that you can book lab test online without any hassle.
 
 Apart from these tests, we have also curated a number of packages for chronic and preventive diseases that one can go for, in time of need. You can also explore our curated packages for annual health checkups with blood sample collection from home.</p>
 
-<h5 className='doctorl'>Additional Services</h5>
-<h6 className='Color'>Expert Doctor Consultations:</h6> <p>Connect with 70+ specialists for personalised care. Symptom Checker: Understand your symptoms with ease and precision. Medicine Orders: Get your order delivered to your doorstep. Digital Health Records: Effortlessly digitise your physical records for easy access. With our comprehensive range of services, there's no need to go anywhere else! Experience the convenience of complete health care at your fingertips.</p>
+<h6 className='doctorl'>Additional Services</h6>
+<h6 className='Color'>Expert Doctor Consultations:</h6> <p>Connect with 70+ specialists for personalised MAD. Symptom Checker: Understand your symptoms with ease and precision. Medicine Orders: Get your order delivered to your doorstep. Digital Health Records: Effortlessly digitise your physical records for easy access. With our comprehensive range of services, there's no need to go anywhere else! Experience the convenience of complete health MAD at your fingertips.</p>
 
   </div>
 );
